7edfd87b52da93d772275636a9ac8c7e82d4c9ac
yurabh <yurabh1990@gmail.com>
2022-08-19 14:08:53 +0300

    add some configuration

--- src/main/java/com/example/learn/microservices/configuration_service/ConfigurationServiceApplication.java
+++ src/main/java/com/example/learn/microservices/configuration_service/ConfigurationServiceApplication.java
@@ -11,5 +11,4 @@ public class ConfigurationServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigurationServiceApplication.class, args);
     }
-
 }
